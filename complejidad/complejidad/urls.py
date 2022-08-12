ff996b3ca504289140b60c1a4aaa886223a0acfc
cesar becerra <sezaru@hotmail.es>
2022-08-11 22:37:09 -0500

    creado template

--- complejidad/complejidad/urls.py
+++ complejidad/complejidad/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from back import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('calculador/', views.calculador),
+    path('', views.home),
 ]
