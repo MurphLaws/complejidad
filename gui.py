5fb88e3a9882fb3a91141900e4316edfd4dad274
cesar becerra <sezaru@hotmail.es>
2022-08-14 18:18:40 -0500

    añadido boton abrir

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter
 from test import apply_model
+from tkinter import filedialog
 
 def enviar_datos():
     texto_n = caja_texto_n.get()
@@ -9,8 +10,14 @@ def enviar_datos():
     respuesta_ubicacion["text"]= respuestas[0]
     respuesta_distancia["text"]= respuestas[1]
 
+def abrir_archivo():
+    archivo =filedialog.askopenfilename(title="Abrir")
+    print(archivo)
+    return archivo
+
 ventana = tkinter.Tk()
 ventana.geometry("400x300")
+
 etiqueta_n = tkinter.Label(ventana, text = "N")
 caja_texto_n = tkinter.Entry(ventana)
 etiqueta_m = tkinter.Label(ventana, text = "M")
@@ -20,6 +27,8 @@ caja_texto_coordenadas = tkinter.Entry(ventana)
 boton_enviar =tkinter.Button(ventana,text="Click", command=enviar_datos)
 respuesta_ubicacion =tkinter.Label(ventana)
 respuesta_distancia =tkinter.Label(ventana)
+boton_abrir =tkinter.Button(ventana,text="Abrir", command=abrir_archivo)
+
 etiqueta_n.pack()
 caja_texto_n.pack()
 etiqueta_m.pack()
@@ -29,6 +38,7 @@ caja_texto_coordenadas.pack()
 boton_enviar.pack()
 respuesta_ubicacion.pack()
 respuesta_distancia.pack()
+boton_abrir.pack()
 
 ventana.mainloop()
 
